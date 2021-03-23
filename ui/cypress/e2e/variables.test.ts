0c5e4d0164bdad0cb9822d086e5a365aee96cc92
Will Baker <wbaker@gmail.com>
2021-03-23 15:25:11 -0400

    variables tests pass

--- ui/cypress/e2e/variables.test.ts
+++ ui/cypress/e2e/variables.test.ts
@@ -113,7 +113,7 @@ describe('Variables', () => {
     cy.getByTestID('context-delete-menu')
       .first()
       .click({force: true})
-    cy.getByTestID('context-delete-variable')
+    cy.getByTestID(`context-delete-variable ${variableName}`)
       .first()
       .click({force: true})
 
